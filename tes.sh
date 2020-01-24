21e3058035723a38b097278c4dc49cff0ec08967
Atomanti007 <atomanti007@citromail.hu>
2020-01-24 13:44:01 +0100

    Create tes.sh

---
+++ tes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/Atomanti007/kubernetes-skeleton/master/kube-flannel.yaml -O ~/kubernetes/kube-flannel.yaml
+wget https://raw.githubusercontent.com/Atomanti007/kubernetes-skeleton/master/dashboard.yaml -O ~/kubernetes/dashboard.yaml
