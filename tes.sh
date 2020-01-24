baa8e639cc6efe1285f12df066e1444cf2827875
Atomanti007 <atomanti007@citromail.hu>
2020-01-24 13:47:07 +0100

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+mkdir ~/kubernetes
+
 wget https://raw.githubusercontent.com/Atomanti007/kubernetes-skeleton/master/kube-flannel.yaml -O ~/kubernetes/kube-flannel.yaml
 wget https://raw.githubusercontent.com/Atomanti007/kubernetes-skeleton/master/dashboard.yaml -O ~/kubernetes/dashboard.yaml
