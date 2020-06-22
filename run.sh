b78bef29dca28eb700b224e6c9f3cabba1db49a5
atomanti007 <atomanti007@citromail.hu>
2020-06-23 00:16:34 +0200

    Update

--- run.sh
+++ run.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+
 sudo apt update -y \
 && sudo apt install docker.io -y \
-&& docker --version \
 && sudo systemctl enable docker \
-&& curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add \
-&& sudo apt install curl -y \
-&& sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" \
-&& sudo apt install kubeadm -y \
-&& kubeadm version \
+&& sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl \
+&& curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - \
+&& echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list \
+&& sudo apt-get update \
+&& sudo apt-get install -y kubectl kubelet kubeadm \
+&& sudo apt-get install bash-completion \
+&& echo 'source <(kubectl completion bash)' >>~/.bashrc \
 && sudo swapoff -a \
 && sudo hostnamectl set-hostname master-node \
-&& sudo kubeadm init --pod-network-cidr=10.244.0.0/16 \
+&& sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$(hostname -I | awk '{print $1}') \
 && mkdir -p $HOME/.kube \
 && sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config \
 && sudo chown $(id -u):$(id -g) $HOME/.kube/config \
-&& wget --no-cache -O ~/kubernetes/kube-flannel.yaml https://raw.githubusercontent.com/Atomanti007/kubernetes-skeleton/master/kube-flannel.yaml \
-&& sudo kubectl apply -f ~/kubernetes/kube-flannel.yaml \
-&& wget --no-cache -O ~/kubernetes/dashboard.yaml https://raw.githubusercontent.com/Atomanti007/kubernetes-skeleton/master/dashboard.yaml \
-&& sudo kubectl apply -f ~/kubernetes/dashboard.yaml
+&& kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" \
+&& kubectl apply -f https://raw.githubusercontent.com/Atomanti007/kubernetes-skeleton/master/dashboard.yaml \
+&& kubectl taint nodes --all node-role.kubernetes.io/master-
